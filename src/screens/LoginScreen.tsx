32a14c1a963e9dc234b36d94f7e17087323e071e
SOTI\tbasumatary <thulunga.basumatary@soti.net>
2023-10-24 13:59:06 +0530

    Changed js to tsx and added signin and signup function

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import { Text } from "react-native-paper";
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import TextInput from "../components/TextInput";
+import BackButton from "../components/BackButton";
+import { theme } from "../core/theme";
+import { emailValidator } from "../helpers/emailValidator";
+import { passwordValidator } from "../helpers/passwordValidator";
+import { dokhonaAuth } from "../../FirebaseCofig";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "@firebase/auth";
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [password, setPassword] = useState({ value: "", error: "" });
+  const [loading, setLoading] = useState(false);
+
+  const signIn =async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(dokhonaAuth, email.value, password.value);
+      console.log(response);
+    } catch (error) {
+      console.log(error)
+      alert
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const signUp =async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(dokhonaAuth, email.value, password.value);
+      console.log(response);
+    } catch (error) {
+      console.log(error)
+      alert
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const onLoginPressed = () => {
+    const emailError = emailValidator(email.value);
+    const passwordError = passwordValidator(password.value);
+    if (emailError || passwordError) {
+      setEmail({ ...email, error: emailError });
+      setPassword({ ...password, error: passwordError });
+      return;
+    }
+    navigation.reset({
+      index: 0,
+      routes: [{ name: "Dashboard" }],
+    });
+  };
+
+  return (
+    <Background>
+      <BackButton goBack={navigation.goBack} />
+      <Logo />
+      <Header>Welcome back.</Header>
+      <TextInput
+        label="Email"
+        returnKeyType="next"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: "" })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+        description={undefined}
+      />
+      <TextInput
+        label="Password"
+        returnKeyType="done"
+        value={password.value}
+        onChangeText={(text) => setPassword({ value: text, error: "" })}
+        error={!!password.error}
+        errorText={password.error}
+        secureTextEntry
+        description={undefined}
+      />
+      <View style={styles.forgotPassword}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("ResetPasswordScreen")}
+        >
+          <Text style={styles.forgot}>Forgot your password?</Text>
+        </TouchableOpacity>
+      </View>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff"></ActivityIndicator>
+      ) : (
+        <>
+          <Button mode="contained" onPress={onLoginPressed} style={undefined}>
+            Login
+          </Button>
+        </>
+      )}
+      <Button mode="contained" onPress={onLoginPressed} style={undefined}>
+        Login
+      </Button>
+      <View style={styles.row}>
+        <Text>Don’t have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.replace("RegisterScreen")}>
+          <Text style={styles.link}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </Background>
+  );
+}
+
+const styles = StyleSheet.create({
+  forgotPassword: {
+    width: "100%",
+    alignItems: "flex-end",
+    marginBottom: 24,
+  },
+  row: {
+    flexDirection: "row",
+    marginTop: 4,
+  },
+  forgot: {
+    fontSize: 13,
+    color: theme.colors.secondary,
+  },
+  link: {
+    fontWeight: "bold",
+    color: theme.colors.primary,
+  },
+});
